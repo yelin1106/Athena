29f93de40b474713b78a62e084f175d1182b5402
Riley <yelin1106@naver.com>
2021-01-01 15:43:29 +0000

    week9 push

---
+++ codingtest/main.py
@@ -0,0 +1,92 @@
+##1주차
+#import greedy.ch3_1
+#import greedy.ch3_2
+#import greedy.ch3_3
+#import greedy.ch3_4
+
+#import greedy.ch11_1
+#import greedy.ch11_2
+#import greedy.ch11_3
+#import greedy.ch11_4
+#import greedy.ch11_5
+#import greedy.ch11_6
+
+#import baekjoon.level2.no1
+#import baekjoon.level2.no2
+#import baekjoon.level2.no3
+#import baekjoon.level2.no4
+#import baekjoon.level2.no5
+
+
+##2주차
+#import realization.ch4_1
+#import realization.ch4_2
+#import realization.ch4_3
+
+#import baekjoon.level3_for.multiplication_table
+#import baekjoon.level3_for.A_plus_B_3
+
+
+##4주차
+#import baekjoon.level20.search_num
+#import baekjoon.level20.number_card2
+#import baekjoon.level20.cut_lenwire
+#import baekjoon.level20.cut_tree
+#import baekjoon.level20.internet_router
+
+#import baekjoon.level6.sum_of_nth
+#import baekjoon.level6.self_number
+#import baekjoon.level6.hansu
+
+##5주차
+#import baekjoon.level7_string.ascii_code
+#import baekjoon.level7_string.sum_of_num
+#import baekjoon.level7_string.find_alphabet
+#import baekjoon.level7_string.string_repeat
+#import baekjoon.level7_string.word_study
+#import baekjoon.level7_string.word_count
+#import baekjoon.level7_string.sangsu
+#import baekjoon.level7_string.dial
+#import baekjoon.level7_string.croatia_alphabet
+#import baekjoon.level7_string.group_word_checker
+
+##6주차
+#import baekjoon.level8_math1.break_even_point
+#import baekjoon.level8_math1.sugar_delivery
+#import baekjoon.level8_math1.honeycomb
+#import baekjoon.level8_math1.find_fractional_num
+#import baekjoon.level8_math1.snail_want_to_climb
+#import baekjoon.level8_math1.hotel_acm
+#import baekjoon.level8_math1.be_the_president
+#import baekjoon.level8_math1.fly_me_to_the_alpha_centauri
+
+#import baekjoon.level10_recursion.factorial
+#import baekjoon.level10_recursion.fibonacci_num_5
+#import baekjoon.level10_recursion.print_star_10
+#import baekjoon.level10_recursion.tower_of_hanoi
+
+
+##7주차
+#import baekjoon.level9_math2.find_prime_num
+#import baekjoon.level9_math2.prime_number
+#import baekjoon.level9_math2.get_prime_num
+#import baekjoon.level9_math2.escape_from_rectangle
+#import baekjoon.level9_math2.fourth_dot
+
+##8주차
+#import baekjoon.level9_math2.Bertrand_postulate
+#import baekjoon.level9_math2.Goldbach_conjecture
+#import baekjoon.level9_math2.rectangle
+#import baekjoon.level9_math2.taxicab_geometry
+#import baekjoon.level9_math2.turret
+
+#import baekjoon.level11_brute_force.black_jack
+#import baekjoon.level11_brute_force.division_sum
+#import baekjoon.level11_brute_force.physique_volume
+#import baekjoon.level11_brute_force.end_of_earth
+
+#import baekjoon.level15_greedy.coin_0
+
+
+##9주차
+import 
